5829652af937b8e7d16115a42d705af32df2f877
Angela Li <angela@Angelas-MacBook-Pro.local>
2023-07-26 16:05:13 -0700

    deploy

--- my-app/server/app.js
+++ my-app/server/app.js
@@ -21,6 +21,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
+
+require("dotenv").config();
+
+// middleware
+const corsOptions = {
+    origin: "https://assignment5-frontend-nc7u.onrender.com", // frontend URI (ReactJS)
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// connect MongoDB
+
 mongoose.connect('mongodb+srv://angelali6462:ULpthEwc5aDA51Xy@assignment5.ycdcf52.mongodb.net/items');
 
+// route
+app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+});
+
+
+
 module.exports = app;
+
+
