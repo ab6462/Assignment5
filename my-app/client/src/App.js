1ff4e871e4894e3b59d86ce8bcadaf073131019a
Angela Li <angela@Angelas-MacBook-Pro.local>
2023-07-26 15:32:13 -0700

    backend deploy

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://assignment5-l9ap.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
