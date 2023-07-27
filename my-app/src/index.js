6379c7f1938e5726fb143a40e549cae8446ac796
Angela Li <angela@Angelas-MacBook-Pro.local>
2023-07-26 17:14:52 -0700

    report

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import listReducer from './reducers/listReducer';
