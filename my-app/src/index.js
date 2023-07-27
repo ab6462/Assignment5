00649aeb1ba5a5cb96cbc6debddd24faf2b5a70a
Angela Li <angela@Angelas-MacBook-Pro.local>
2023-07-26 17:15:42 -0700

    re

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore} from 'redux';
 import { Provider } from 'react-redux'
 import listReducer from './reducers/listReducer';
 
